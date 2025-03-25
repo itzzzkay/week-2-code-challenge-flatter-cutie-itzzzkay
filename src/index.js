373c805c49a7747a8c97b2e74f15842e308b79ad
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-03-25 14:43:43 +0300

    character details function complete

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-document.addEventListener("DOMContentLoaded", getCharacters)
+document.addEventListener("DOMContentLoaded", getCharacters);
 
 function getCharacters() {
-    fetch("https://anything-you-want-blond.vercel.app/characters")
-    .then(response => response.json())
-    .then(data => {
+  fetch("https://anything-you-want-blond.vercel.app/characters")
+    .then((response) => response.json())
+    .then((data) => {
+      const characterBar = document.getElementById("character_bar");
+      characterBar.innerHTML = "";
 
-        const characterBar = document.getElementById("character_bar")
-        characterBar.innerHTML=""
-
-        data.forEach(character => {
-            const span = document.createElement("span")
-            span.textContent = character.name
-            span.addEventListener("click",()=>showCharacterDetails(character));
-            characterBar.appendChild(span);
-        });
+      data.forEach((character) => {
+        const span = document.createElement("span");
+        span.textContent = character.name;
+        span.addEventListener("click", () => characterDetails(character));
+        characterBar.appendChild(span);
+      });
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
+}
+
+function characterDetails() {
+  const name = document.getElementById("name");
+  const image = document.getElementById("image");
+  const votes = document.getElementById("vote-count");
+
+  name.textContent = character.name;
+  image.src = character.image;
+  image.alt = character.name;
+  votes.textContent = character.votes;
 }
