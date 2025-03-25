ab93e6931d4efbf92e01d093e7a64a12538ea935
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-03-25 12:19:37 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// Your code here
+function xters(){
+    fetch ("https://anything-you-want-blond.vercel.app/characters")
+    .then(response => response.json())
+    .then(data =>
+
+    )
+    .catch(error => console.error(error));
+
+    if(!response.ok){
+        throw new Error("You clearly don't how to accessthe server data") 
+    }
+}
+
+function displayCharacters(){
+    let character_bar = document.getElementById('character-bar')
+}
+const myUrl = "https://anything-you-want-blond.vercel.app/characters"
